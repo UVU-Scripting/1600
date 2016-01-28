df9b1d19b45026e6c2b644b87946d5615e3cc709
Mike Cowley <Mykie92@Mikes-Air.ad.uvu.edu>
2016-01-28 10:50:49 -0700

    Am I supposed to commit and push the whole Project? There's a lot of files.

---
+++ Loops _mikeCowley/Assets/Scripts/forEachLoop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class forEachLoop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string[] strings = new string[6];
+		//array of 4 items
+		print ("This is my Grocery List");
+		strings [0] = "Milk";
+		strings [1] = "Navel Oranges";
+		strings [2] = "Cocoa Puffs Cereal";
+		strings [3] = "Can of Black Beans";
+		strings [4] = "Donuts";
+		strings [5] = "Butter";
+
+		foreach (string item in strings) {
+			print (item);
+		}
+	}
+}
---
+++ Loops _mikeCowley/Assets/Scripts/whileLoop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class whileLoop : MonoBehaviour {
+
+	int Goombas = 11;
+	
+	// Use this for initialization
+	void Start () {
+		while (Goombas >= 0 ) {
+			Debug.Log ("One Goomba has been smashed!" + Goombas);
+			
+			Goombas --;
+		}
+		print("All the Goombas are smashed...Go Mario!");
+	}
+}

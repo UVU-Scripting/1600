5661472605a68c58856dea04563912869bb0fd7f
Carlton Baxter <carltongregbaxter@gmail.com>
2016-01-28 13:51:47 -0700

    My Loops Assignment

---
+++ Loops_carltonBaxter/Assets/Scripts/ForEachLoop.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForEachLoop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		{
+			print ("This is my Grocery List");
+		}
+	
+			string[] strings = new string[6];
+			//array of 6 items
+			strings [0] = "Milk";
+			strings [1] = "Navel Oranges";
+			strings [2] = "Cocoa Puffs Cereal";
+			strings [3] = "Can of Black Beans";
+			strings [4] = "Donuts";
+			strings [5] = "Butter";
+			
+			foreach (string item in strings) 
+			{
+				print (item);
+			}
+	}
+	
+
+}
---
+++ Loops_carltonBaxter/Assets/Scripts/WhileLoop.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoop : MonoBehaviour {
+	
+		int goombaCount = 12;
+		
+		// Use this for initialization
+		void Start () 
+		{
+			while (goombaCount > 0) {
+				Debug.Log ("One Goomba has been smashed! Goomba count 11");
+				
+			goombaCount --;
+			Debug.Log ("One Goomba has been smashed! Goomba count 10");
+			goombaCount --;
+			Debug.Log ("One Goomba has been smashed! Goomba count 9");
+			goombaCount --;
+			Debug.Log ("One Goomba has been smashed! Goomba count 8");
+			goombaCount --;
+			Debug.Log ("One Goomba has been smashed! Goomba count 7");
+			goombaCount --;
+			Debug.Log ("One Goomba has been smashed! Goomba count 6");
+			goombaCount --;
+			Debug.Log ("One Goomba has been smashed! Goomba count 5");
+			goombaCount --;
+			Debug.Log ("One Goomba has been smashed! Goomba count 4");
+			goombaCount --;
+			Debug.Log ("One Goomba has been smashed! Goomba count 3");
+			goombaCount --;
+			Debug.Log ("One Goomba has been smashed! Goomba count 2");
+			goombaCount --;
+			Debug.Log ("One Goomba has been smashed! Goomba count 1");
+			goombaCount --;
+			Debug.Log ("One Goomba has been smashed! Goomba count 0");
+			goombaCount --;
+		
+		
+		
+		}
+		{
+			print ("All of the Goombas are smashed... Go Mario!");
+		}
+
+	}
+
+}

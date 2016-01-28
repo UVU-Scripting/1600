012ec3edb6a6ea177f45522fd9831a7a8ca8199c
Kevin Clemons <kolibermedia@gmail.com>
2016-01-28 16:48:13 -0700

    Added Loop assignment as a unity project.

---
+++ Loops_kevinClemons/Assets/Scripts/ForEachLoop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForEachLoop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		string[] groceryList = new string[] {"Milk", "Navel Oranges", "Cocoa Puffs Cereal",
+												"Can of Black Beans", "Donuts", "Butter"};
+
+		Debug.Log("This is my Grocery List");
+		foreach (string grocery in groceryList) {
+			Debug.Log(grocery);
+		}
+	}
+}
---
+++ Loops_kevinClemons/Assets/Scripts/WhileLoop.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		int goombas = 12;
+
+		while (goombas > 0) {
+			Debug.Log("One Goomba has been smashed! Goomba count " + --goombas);
+		}
+		Debug.Log("All the Goombas are smashed...Go Mario!");
+	}
+}

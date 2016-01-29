6f8dc97f3a8d3332dedb8d689e5ae9ae9f40a548
Khristopher Rhodes <10641486@CS518-07.ad.uvu.edu>
2016-01-29 09:56:47 -0700

    LoopAssignment homework... thing.

---
+++ Loops_KhrisRhodes/Assets/Scripts/forEachLoopGrocery.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class forEachLoopGrocery : MonoBehaviour 
+{
+	string intro = "This is my shopping list.";
+	string theEnd = "Gonna have a good breakfast tomorrow!";
+	// Use this for initialization
+	void Start () 
+	{
+		print(intro);
+
+		string[] groceryList = new string[6];
+		groceryList [0] = "Milk";
+		groceryList [1] = "Orange juice";
+		groceryList [2] = "Coco Puffs";
+		groceryList [3] = "Eggs";
+		groceryList [4] = "Donuts";
+		groceryList [5] = "Bacon";
+
+		foreach (string item in groceryList) 
+		{
+			print (item);
+		}
+
+		print(theEnd);
+	}
+}
---
+++ Loops_KhrisRhodes/Assets/Scripts/whileLoopGoomba.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class whileLoopGoomba : MonoBehaviour 
+{
+	string intro = "There are 12 Goombas and Mario is going to smash them all!";
+	string victory = "All the Goombas are smashed...Go Mario!";
+	int goombasRemaining = 12;
+	int goombasSmashed = 1;
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log (intro);
+		while (goombasRemaining > 0) 
+		{
+			goombasRemaining --;
+			Debug.Log (goombasSmashed + " goomba has been smashed! Goomba count is " + goombasRemaining);
+			goombasSmashed ++;
+		}
+		Debug.Log (victory);
+	}
+}

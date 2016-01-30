68cb378492f582c3773c9d58a682f89739c3cef0
Jada Scott <jadalscott@hotmail.com>
2016-01-30 10:42:55 -0700

    Trying again . . . .

---
+++ Loops_jadaScott/Assets/foreachloop.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class foreachloop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		string[] strings = new string[7];
+		//array of 4 items
+		strings[0] ="This is my Grocery List";
+		strings[1] ="Milk";
+		strings[2] ="Navel Oranges";
+		strings[3] ="Cocoa Puffs Cereal";
+		strings[4] ="Can of Black Beans";
+		strings[5] ="Donuts";
+		strings[6] ="Butter";
+
+		foreach (string item in strings) 
+		{
+			print (item);
+		}
+	}
+
+
+}
---
+++ Loops_jadaScott/Assets/whileloop.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class whileloop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		string[] strings = new string[13];
+		//array of 4 items
+		strings[0] ="One Goomba has been smashed! Goomba count 11";
+		strings[1] ="One Goomba has been smashed! Goomba count 10";
+		strings[2] ="One Goomba has been smashed! Goomba count 9";
+		strings[3] ="One Goomba has been smashed! Goomba count 8";
+		strings[4] ="One Goomba has been smashed! Goomba count 7";
+		strings[5] ="One Goomba has been smashed! Goomba count 6";
+		strings[6] ="One Goomba has been smashed! Goomba count 5";
+		strings[7] ="One Goomba has been smashed! Goomba count 4";
+		strings[8] ="One Goomba has been smashed! Goomba count 3";
+		strings[9] ="One Goomba has been smashed! Goomba count 2";
+		strings[10] ="One Goomba has been smashed! Goomba count 1";
+		strings[11] ="One Goomba has been smashed! Goomba count 0";
+		strings[12] ="All the Goombas are smashed...Go Mario!";
+
+		foreach (string item in strings) 
+		{
+			print (item);
+		}
+	}
+
+
+}

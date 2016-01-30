38e8e3c416f267aac2eb5815a3573d11e3e7074c
10720117 <jeromeangeles1996@gmail.com>
2016-01-29 17:58:37 -0700

    This is my loop project

---
+++ loop project Jerome Angeles/Assets/script/whileLoop.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class whileLoop : MonoBehaviour {
+	int goombaCount = 11;
+	// Use this for initialization
+	void Start ()
+	{
+		while (goombaCount > 0) 
+		{
+			Debug.Log("One Goomba has been smashed! Goomba count " + goombaCount);
+			goombaCount--;
+
+		}
+		{
+			bool shouldContinue = false;
+		
+			do 
+			{
+				print ("All the Goombas are smashed...Go Mario!");
+			} 
+			while (shouldContinue == true);
+		}
+	}
+}

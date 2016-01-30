cdc70ecb509b7740dbc93d1bc37543cafcd9e4ec
Parker Francis <parkermfrancis@gmail.com>
2016-01-30 10:07:05 -0700

    loopAssigment_parkerFrancis

---
+++ Assets/Archive/goompaLoop.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goompaLoop : MonoBehaviour {
+
+	int goombas = 12;
+	// Use this for initialization
+	void Start () 
+	{
+		while (goombas > 0) 
+		{
+			Debug.Log ("one goombas has been smashed!" + goombas);
+			goombas --;
+		}
+		bool shouldContinue = false;
+		do {
+			print ("All goombas are smashed...Go Mario!");
+		} 
+		while(shouldContinue == true);
+	}
+}
---
+++ Assets/Archive/groceryListExample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class groceryListExample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string[] thisIsMyGroceryList = new string[6];
+		//array of 6 items
+		thisIsMyGroceryList[0] = "Milk";
+		thisIsMyGroceryList[1] = "Navel Oranges";
+		thisIsMyGroceryList[2] = "Coco Puffs Cereal";
+		thisIsMyGroceryList[3] = "Can of Black Beans";
+		thisIsMyGroceryList[4] = "Donuts";
+		thisIsMyGroceryList[5] = "Butter";
+		
+		foreach (string item in thisIsMyGroceryList) 
+		{
+			print (item);
+		}
+	}
+
+}

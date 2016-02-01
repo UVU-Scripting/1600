588511a2de30267cfb900e025fdaf2f781bf0849
Sawyer Harr <10626189@uvlink.uvu.edu>
2016-01-31 17:10:05 -0700

    Loops Assignment (Goomba Squish and Grocery List).

---
+++ Loops_sawyerHarr/Loops_sawyerHarr/Assets/Scripts/goombaCrush.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goombaCrush : MonoBehaviour {
+
+	int goombasNotSquished = 5;
+	void Start () {
+		while (goombasNotSquished >= 0)
+		{
+			if (goombasNotSquished > 1) {
+			Debug.Log ("Mario squished a Goomba! There are " + goombasNotSquished + " remaining!");
+			goombasNotSquished--;
+			}
+			else if (goombasNotSquished == 1) {
+			Debug.Log ("Mario squished a Goomba! There is " + goombasNotSquished + " remaining!");
+			goombasNotSquished--;
+			}
+			else if (goombasNotSquished == 0) {
+			Debug.Log ("The Goomba Squished Mario! That sucks! There are " + goombasNotSquished + " Marios remaing. Game over");
+			goombasNotSquished--;
+			}
+		}
+	}	
+}
+

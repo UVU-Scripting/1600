29b94c2a2c0e942841a6d172b01c2ba327a9c105
Khristopher Rhodes <10641486@CS518-07.ad.uvu.edu>
2016-02-10 14:19:41 -0700

    Phase 1 completed

---
+++ khrisRhodes_tallyApp/Assets/Scripts/tallyCounter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tallyCounter : MonoBehaviour 
+{
+	int tally = 0;
+
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			tally++;
+			print (tally);
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) 
+		{
+			if (tally > 0)
+			{
+				tally--;
+				print (tally);
+			}
+
+			else
+			{
+				print (tally);
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.R)) 
+		{
+			tally = 0;
+			print (tally);
+		}
+	}
+}

f9bead4f4ea9ec53204a88b4d9413807be42778c
Carlton Baxter <carltongregbaxter@gmail.com>
2016-02-17 16:58:18 -0700

    Phase 2 of the Tally App project.  The zip file wouldn't push.

---
+++ Tally App/Assets/New Folder/Text.cs
@@ -0,0 +1,22 @@
+﻿/*using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Text : MonoBehaviour {
+
+	public static int tallyInt;
+	Text text;
+	
+	// Use this for initialization
+	void Awake () 
+	{
+		text = GetComponent<Text> ();
+		tallyInt = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		text.text = "Tally: " + tallyInt.ToString ();
+	}
+}*/

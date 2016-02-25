1d1a31957e2481d33670540d3bb24bc50136de54
Khristopher Rhodes <10641486@CS518-07.ad.uvu.edu>
2016-02-25 08:42:13 -0700

    complete with rotation.

---
+++ khrisRhodes_PlatformProject/Assets/Scripts/camuraControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camuraControl : MonoBehaviour 
+{
+	public Transform target;
+	public float distance = -10;
+	public float lift = 1.5f;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = target.position + new Vector3(0, lift, distance);
+	}
+}

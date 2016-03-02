79f6e2239983811aead29f2ce0c13e2fe273d81f
rosetejano <rosehartvigsen@gmail.com>
2016-03-01 23:34:01 -0700

    Added files via upload
    i tried to do this app 3 times at home. it worked perfectly fine at home. im not sure why its not 100% working now

---
+++ newgameapptry2/Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float speed = 8.0f;
+
+	public float jumpSpeed = 7.0f;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		var move = new Vector3 (Input.GetAxis("Horizontal"), Input.GetAxis ("Vertical"), 0);
+		                        transform.position += move * jumpSpeed * Time.deltaTime;
+
+		    //Jump with spacebar
+		    if (Input.GetKeyDown (KeyCode.Space))
+		    {
+				GetComponent<Rigidbody> ().velocity += Vector3.up * jumpSpeed;
+			}
+
+	}
+}
